e8d33ad5e4555e6bed712a4e97f7a8fecb6104ff
Niharika Srivastav <niharika@Niharikas-MacBook-Pro.local>
2020-08-01 16:23:55 -0700

    datagen file commit

--- simple-recommender/scripts/datagen.py
+++ simple-recommender/scripts/datagen.py
@@ -1,33 +1,36 @@
+import os
 class DataGen() :
+
     def users(self, obsFname, unobsFname) :
         obsFile = open(obsFname, "r")
         unobsFile = open(unobsFname, "r")
-        userDict = set()
-        itemsDict = set()
+        userset = set()
+        itemset = set()
         usersFile = open("users.txt","w+")
         itemsFile = open("items.txt", "w+")
 
         f1 = obsFile.readlines()
         for i in f1 :
             row = i.split()
-            if row[0] not in userDict:
-                userDict.add(row[0])
-            if row[1] not in itemsDict:
-                itemsDict.add(row[1])
+            userset.add(row[0])
+            itemset.add(row[1])
 
         f2 = unobsFile.readlines()
         for i in f2 :
             row = i.split()
-            if row[0] not in userDict:
-                userDict.add(row[0])
-            if row[1] not in itemsDict:
-                itemsDict.add(row[1])
+            userset.add(row[0])
+            itemset.add(row[1])
 
-        for i in userDict:
+        for i in userset:
             usersFile.write("%s\r\n" %(i))
 
-        for i in itemsDict:
+        for i in itemset:
             itemsFile.write("%s\r\n" %(i))
 
 dataObject = DataGen()
-dataObject.users("rating_obs.txt", "rating_uno.txt")
+cur_path = os.getcwd()
+print(cur_path)
+os.chdir('../')
+new_path = os.getcwd() #os.path.relpath('../data/simple-recommender/0/eval/', cur_path)
+print(new_path)
+dataObject.users(new_path+"/data/simple-recommender/0/eval/rating_obs.txt", new_path+"/data/simple-recommender/0/eval/rating_uno.txt")
