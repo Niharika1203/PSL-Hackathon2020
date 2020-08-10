f83b343921d5768db9cb93148921da39e09025ea
Yatong Chen <yatong@Yatongs-MacBook-Pro.local>
2020-08-10 15:48:24 -0700

    remove weight learning

--- simple-recommender/cli/run.sh
+++ simple-recommender/cli/run.sh
@@ -8,8 +8,11 @@ readonly PSL_VERSION='2.2.1'
 readonly JAR_PATH="./psl-cli-${PSL_VERSION}.jar"
 readonly BASE_NAME='simple-recommender'
 
-readonly ADDITIONAL_PSL_OPTIONS=''
-readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval org.linqs.psl.evaluation.statistics.DiscreteEvaluator'
+
+readonly ADDITIONAL_PSL_OPTIONS='-int-ids'
+readonly ADDITIONAL_LEARN_OPTIONS='--learn'
+readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval org.linqs.psl.evaluation.statistics.ContinuousEvaluator'
+
 
 function main() {
    trap exit SIGINT
@@ -22,17 +25,23 @@ function main() {
 
    # Run PSL
    runEvaluation "$@"
+
 }
+
+
+
 
 function runEvaluation() {
    echo "Running PSL Inference"
 
-   java -jar "${JAR_PATH}" --model "${BASE_NAME}.psl" --data "${BASE_NAME}.data" --postgres niharika --output inferred-predicates ${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
+   java -jar "${JAR_PATH}" --model "${BASE_NAME}-learned.psl" --data "${BASE_NAME}-eval.data" --output inferred-predicates ${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
    if [[ "$?" -ne 0 ]]; then
       echo 'ERROR: Failed to run infernce'
       exit 70
    fi
 }
+
+
 
 function check_requirements() {
    local hasWget
