3ddd4939bc9334b77950a25dcf6eecfa3edd6a9e
Yatong Chen <yatong@Yatongs-MacBook-Pro.local>
2020-08-09 18:21:51 -0700

    add weight learning

--- simple-recommender_small/cli/run.sh
+++ simple-recommender_small/cli/run.sh
@@ -8,8 +8,11 @@ readonly PSL_VERSION='2.2.1'
 readonly JAR_PATH="./psl-cli-${PSL_VERSION}.jar"
 readonly BASE_NAME='simple-recommender'
 
-readonly ADDITIONAL_PSL_OPTIONS=''
-readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval org.linqs.psl.evaluation.statistics.DiscreteEvaluator'
+
+readonly ADDITIONAL_PSL_OPTIONS='-int-ids'
+readonly ADDITIONAL_LEARN_OPTIONS='--learn'
+readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval org.linqs.psl.evaluation.statistics.ContinuousEvaluator'
+
 
 function main() {
    trap exit SIGINT
@@ -21,18 +24,35 @@ function main() {
    fetch_psl
 
    # Run PSL
+   runWeightLearning "$@"
    runEvaluation "$@"
+
 }
+
+
+
+function runWeightLearning() {
+   echo "Running PSL Weight Learning"
+
+   java -jar "${JAR_PATH}" --model "${BASE_NAME}.psl" --data "${BASE_NAME}-learn.data" ${ADDITIONAL_LEARN_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: Failed to run weight learning'
+      exit 60
+   fi
+}
+
 
 function runEvaluation() {
    echo "Running PSL Inference"
 
-   java -jar "${JAR_PATH}" --model "${BASE_NAME}.psl" --data "${BASE_NAME}.data" --output inferred-predicates ${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
+   java -jar "${JAR_PATH}" --model "${BASE_NAME}-learned.psl" --data "${BASE_NAME}-eval.data" --output inferred-predicates ${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
    if [[ "$?" -ne 0 ]]; then
       echo 'ERROR: Failed to run infernce'
       exit 70
    fi
 }
+
+
 
 function check_requirements() {
    local hasWget
